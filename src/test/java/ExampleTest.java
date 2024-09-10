7274ba03e572c44a62add50fdbf93ebd171ceecc
Cristian Mello <cristianc.mello@gmail.com>
2024-09-09 22:25:01 -0300

    feat: adicionando examplo de teste

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void nothing() {
+        Assertions.assertTrue(true);
+    }
+}
